9d2d2f3fa3befdfba26724300e50d24c56a95113
Heraved <48395159+Heraved@users.noreply.github.com>
2019-06-07 14:54:46 +0200

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziennik
+{
+    class Menu
+    {
+        public static void MenuDziennika()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. priting student");
+                Console.WriteLine("2. adding student");
+                Console.WriteLine("3. exit");
+
+                ConsoleKeyInfo info = Console.ReadKey();
+
+                if (info.Key == ConsoleKey.D3) ;
+                {
+                    Environment.Exit(0); break;
+
+                }
+                if (info.Key == ConsoleKey.D1)
+                {
+                    Console.WriteLine("In progress!");
+
+                }
+                else if (info.Key == ConsoleKey.D2) ;
+                {
+                    Console.WriteLine("In progress!");
+                }
+            }
+        }
+
+    }
+}
